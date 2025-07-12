25941ce9fff08cceafd9959df5c29064dd580706
fitlgui <guilherme.fitl10@gmail.com>
2025-07-12 13:54:05 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductCardComponent } from './shareds/product-card/product-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'catalog', component: HomeComponent },
+  { path: 'catalog/:id', component: ProductCardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './shareds/product-card/product-card.component';
+import { BannerComponentComponent } from './shareds/banner-component/banner-component.component';
+import { HomeComponent } from './components/home/home.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardComponent,
+    BannerComponentComponent,
+    HomeComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
