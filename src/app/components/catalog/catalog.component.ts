42652b70e99755437486eeab5c013e0da8dbae2c
Guilherme Fitl De Carvalho <135091807+fitlgui@users.noreply.github.com>
2025-07-15 12:21:40 -0400

    adjust catalog and add features

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,10 +1,139 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  // Variavel para armazenar os produtos
+  public allProducts: any[] = [];
+  public products: any[] = [];
 
+  // Variavel para armazenar as categorias
+  public categories: any[] = [];
+
+  // Variavel para armazenar as marcas
+  public brands: any[] = [];
+
+  // Variavel para armazenar os tamanhos
+  public sizes: any[] = [];
+
+  // Variavel para armazenar as cores
+  public colors: any[] = [];
+
+  public selectedCategories: Set<string> = new Set();
+  public selectedBrands: Set<string> = new Set();
+  public selectedSizes: Set<string> = new Set();
+  public selectedColors: Set<string> = new Set();
+
+
+  constructor(private readonly productsService: ProductsService) {}
+
+  ngOnInit(){
+
+    // Buscando Todos os Produtos
+    this.productsService.getProducts().subscribe((data: any[]) => {
+      this.allProducts = data;
+      this.products = data
+    })
+
+    // Buscar Todas as Categorias
+    this.productsService.getCategories().subscribe((data: any[]) => {
+      this.categories = data
+    })
+
+    // Buscar Todas as Marcas
+    this.productsService.getProducts().subscribe((data: any[]) => {
+      // Se for igual retirar a repetição
+      this.brands = [...new Set(data.map(product => product.brand))];
+      return this.brands;
+    });
+
+    // Buscar Todos os Tamanhos
+    this.productsService.getSize().subscribe((data: any[]) => {
+      this.sizes = data
+    });
+
+    // Buscar Todas as Cores
+    this.productsService.getColors().subscribe((data: any[]) => {
+      this.colors = data;
+    });
+  }
+
+  private applyFilters() {
+    let filteredProducts = [...this.allProducts];
+
+    if (this.selectedCategories.size > 0) {
+      filteredProducts = filteredProducts.filter(product => this.selectedCategories.has(product.category));
+    }
+
+    if (this.selectedBrands.size > 0) {
+      filteredProducts = filteredProducts.filter(product => this.selectedBrands.has(product.brand));
+    }
+
+    if (this.selectedSizes.size > 0) {
+      filteredProducts = filteredProducts.filter(product => this.selectedSizes.has(product.size));
+    }
+
+    if (this.selectedColors.size > 0) {
+      filteredProducts = filteredProducts.filter(product => this.selectedColors.has(product.color));
+    }
+
+    this.products = filteredProducts;
+  }
+
+  // Mostrar os produtos por categoria
+  getProductsCategory(event: any, category: string) {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      this.selectedCategories.add(category);
+    } else {
+      this.selectedCategories.delete(category);
+    }
+    this.applyFilters();
+  }
+
+  // Mostrar os produtos por marca
+  getProductBrand(event: any, brand: string) {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      this.selectedBrands.add(brand);
+    } else {
+      this.selectedBrands.delete(brand);
+    }
+    this.applyFilters();
+  }
+
+  // Mostrar os produtos por tamanho
+  getProductSize(event: any, size: string) {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      this.selectedSizes.add(size);
+    } else {
+      this.selectedSizes.delete(size);
+    }
+    this.applyFilters();
+  }
+
+  // Mostrar os produtos por cor
+  getProductColor(color: string) {
+    if (this.selectedColors.has(color)) {
+      this.selectedColors.delete(color);
+    } else {
+      this.selectedColors.add(color);
+    }
+    this.applyFilters();
+  }
+
+  // Mostrar o numero de produtos por Tamanho
+  getNumberOfProductsBySize(size: string): number {
+    return this.products.filter(product => product.size === size).length;
+  }
+
+  // Mostrar o numero de produtos por Cor
+  getNumberOfProductsByColor(color: string): number {
+    return this.products.filter(product => product.color === color).length;
+  }
 }
--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -10,10 +10,39 @@ export class ProductsService {
 
   getProducts(): Observable<any[]> {
     return of([
-      { id: 1, name: 'W-THANE PDA 514', price: 132.00, descont:15, image: '../../../assets/images/cartShoppingTinta.svg' },
-      { id: 2, name: 'W-THANE PDA 515', price: 145.00, descont: 15, image: '../../../assets/images/cartShoppingTinta.svg' },
-      { id: 3, name: 'W-THANE PDA 516', price: 160.00, descont: 15, image: '../../../assets/images/cartShoppingTinta.svg' },
-      { id: 4, name: 'W-THANE PDA 517', price: 175.00, descont: 15, image: '../../../assets/images/cartShoppingTinta.svg' }
+      { id: 1, name: 'W-THANE PDA 514', price: 132.00, category: 'Agua', brand: 'WEG', size: '1L', descont:15, color:'Vermelha', image: '../../../assets/images/cartShoppingTinta.svg' },
+      { id: 2, name: 'W-THANE PDA 515', price: 145.00, category: 'Automovel', brand: 'WMA', size: '1L', descont: 15, color: 'Azul', image: '../../../assets/images/cartShoppingTinta.svg' },
+      { id: 3, name: 'W-THANE PDA 516', price: 160.00, category: 'Industria' , brand: 'WEG', size: '1L', descont: 15, color: 'Verde', image: '../../../assets/images/cartShoppingTinta.svg' },
+      { id: 4, name: 'W-THANE PDA 517', price: 175.00, category: 'Industria', brand: 'WEG', size: '5L', descont: 15, color: 'Amarelo', image: '../../../assets/images/cartShoppingTinta.svg' },
+      { id: 5, name: 'W-THANE PDA 518', price: 190.00, category: 'Automovel', brand: 'WEG', size: '1L', descont: 15, color: 'Preto', image: '../../../assets/images/cartShoppingTinta.svg' },
+      { id: 6, name: 'W-THANE PDA 519', price: 205.00, category: 'Agua', brand: 'WEG', size: '2L', descont: 15, color: 'Branco', image: '../../../assets/images/cartShoppingTinta.svg' },
     ]);
   }
+
+  getCategories(): Observable<any[]> {
+    return of([
+      { id: 1, name: 'Agua', qtd: 3 },
+      { id: 2, name: 'Automovel', qtd: 5 },
+      { id: 3, name: 'Industria', qtd: 2 }
+    ]);
+  }
+
+  getSize(): Observable<any[]> {
+    return of([
+      { id: 1, name: '1L' },
+      { id: 2, name: '2L' },
+      { id: 3, name: '5L' }
+    ]);
+  }
+
+  getColors(): Observable<any[]> {
+      return of([
+        { id: 1, name: 'Vermelha', color: '#FF0000' },
+        { id: 2, name: 'Azul', color: '#0000FF' },
+        { id: 3, name: 'Verde', color: '#00FF00' },
+        { id: 4, name: 'Amarela', color: '#FFFF00' },
+        { id: 5, name: 'Preto', color: '#000000' },
+        { id: 6, name: 'Branco', color: '#FFFFFF' }
+      ]);
+    }
 }
